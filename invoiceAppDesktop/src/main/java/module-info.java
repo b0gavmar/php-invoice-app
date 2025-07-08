82d69037f1ab3020d850e9c3709713d19d07e702
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-07-08 16:09:05 +0200

    javafx app létrehozva

---
+++ invoiceAppDesktop/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.example.invoiceappdesktop {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.invoiceappdesktop to javafx.fxml;
+    exports org.example.invoiceappdesktop;
+}
